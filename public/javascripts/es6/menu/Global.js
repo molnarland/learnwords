d0648b374bb71f0b7247c19376854a6bf57fb8d9
molnarland <rrmolnar@gmail.com>
2017-04-11 09:32:15 +0100

    erase convertSwitchToBoolean method

--- public/javascripts/es6/menu/Global.js
+++ public/javascripts/es6/menu/Global.js
@@ -432,23 +432,4 @@ export default class Global
     {
         document.querySelector(this.selectorOfNavigator).popPage(options);
     }
-
-    /**
-     * @param {string} switchValue - "on" or "off"
-     */
-    convertSwitchToBoolean (switchValue)
-    {
-        switch (switchValue)
-        {
-            case 'on':
-                return true;
-                break;
-            case 'off':
-                return false;
-                break;
-            default:
-                console.warn('Undefined switch value!');
-                break;
-        }
-    }
 }
